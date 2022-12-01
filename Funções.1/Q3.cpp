dc262a8bf83fe1cd0633e9a46484389b863af3c3
ArthurFerrazz <111258580+ArthurFerrazz@users.noreply.github.com>
2022-12-01 10:06:54 -0300

    Quest 3

--- Funções.1/Q3.cpp
+++ Funções.1/Q3.cpp
@@ -1,5 +1,5 @@
-/*Escreva uma função que receba por parâmetro uma temperatura em graus Fahrenheit e a
-retorne convertida em graus Celsius. A fórmula de conversão é: C = (F – 32.0) * (5.0/9.0),
+/*Escreva uma funÃ§Ã£o que receba por parÃ¢metro uma temperatura em graus Fahrenheit e a
+retorne convertida em graus Celsius. A fÃ³rmula de conversÃ£o Ã©: C = (F â€“ 32.0) * (5.0/9.0),
 sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.*/
 
 #include <stdio.h>
@@ -12,7 +12,7 @@ c = (f - 32) * (5.0/9.0);
 }
 int main(){
     double f ;
-    printf("Me forneca a temperatura em Fahrenheit: ");
+    printf("Insira a temperatura em Fahrenheit: ");
     scanf("%lf", &f);
     conversao(f);
 }
