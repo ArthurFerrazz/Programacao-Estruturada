1164a62174598520b13c1e9fcce61678af1c54c4
ArthurFerrazz <111258580+ArthurFerrazz@users.noreply.github.com>
2022-12-01 10:58:51 -0300

    Quest 4

--- Vetor e Matriz/Q4.cpp
+++ Vetor e Matriz/Q4.cpp
@@ -2,25 +2,18 @@
 imprima todos os elementos, exceto os elementos da diagonal principal.*/
 
 #include <stdio.h>
-#include <math.h>
 
 int main(){
-    int num[6][6],i,j;
-    for ( i = 0; i < 6; i++)
-    {
-        for ( j = 0; j < 6; j++)
-        {
-           printf("Me forneça o numero a ser armazenado em [%d] [%d] :", i,j);
-           scanf("%d",&num[i][j]);
-        }
-        
+    int num[6][6], i, j;
+    for ( i = 0; i < 6; i++){
+    for ( j = 0; j < 6; j++){
+        printf("Digite o numero a ser armazenado em [%d] [%d] :", i, j);
+        scanf("%d", &num[i][j]);
+   }
     }
-     for ( i = 1; i < 6; i++)
-    {
-        for (  j= 0; j < 6; j++)
-        {
-            printf("%d\n",num[i][j]);
-        }
-        
+    for ( i = 1; i < 6; i++){
+    for (  j= 0; j < 6; j++){
+        printf("%d\n", num[i][j]);
+   }
     }
-}
+  }
