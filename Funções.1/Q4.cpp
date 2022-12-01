4f050f3f81455cfc76b98b47884f771508848293
ArthurFerrazz <111258580+ArthurFerrazz@users.noreply.github.com>
2022-12-01 10:09:29 -0300

    Quest 4

--- Funções.1/Q4.cpp
+++ Funções.1/Q4.cpp
@@ -1,23 +1,23 @@
-/*Escreva uma funÁ„o que receba por par‚metro a altura e o raio de um cilindro circular e
-retorne o volume desse cilindro. O volume de um cilindro circular È calculado por meio da
-seguinte fÛrmula:                                                  sabendo que p = 3.1414592*/
+/*Escreva uma fun√ß√£o que receba por par√¢metro a altura e o raio de um cilindro circular e
+retorne o volume desse cilindro. O volume de um cilindro circular √© calculado por meio da
+seguinte f√≥rmula:                                                  sabendo que p = 3.1414592*/
 
 #include <stdio.h>
 #include <math.h>
 #define PI 3.1414592
 
-int volume( float altura,float raio ){
+int volume( float altura, float raio ){
 float volume;
-	volume = PI * pow(raio,2)*altura;
+	volume = PI * pow(raio,2) * altura;
 	printf("Volume: %.2f",volume);
 }
 
 int main (){
     float altura ,raio;
-    printf("Me forneca a altura: ");
+    printf("Digite a altura: ");
     scanf("%f",&altura);
     
-    printf("Me forneca a raio: ");
+    printf("Digite o raio: ");
     scanf("%f",&raio);
     volume(altura,raio);
 }
