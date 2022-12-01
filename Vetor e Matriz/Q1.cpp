5ab2b259749ab5d9ea60aa48bac50d1ff03af927
ArthurFerrazz <111258580+ArthurFerrazz@users.noreply.github.com>
2022-12-01 10:21:10 -0300

    Update Q1.cpp
    Quest 1

--- Vetor e Matriz/Q1.cpp
+++ Vetor e Matriz/Q1.cpp
@@ -1,14 +1,13 @@
-/*Escreva um programa  lÍ matricula e as notas de no m·ximo 100
+/*Escreva um programa  l√™ matricula e as notas de no m√°ximo 100
 alunos. O programa deve ler e armazenar uma nova matricula e uma nova nota
-enquanto o usu·rio desejar. ApÛs o armazenamento permita ao usu·rio
+enquanto o usu√°rio desejar. Ap√≥s o armazenamento permita ao usu√°rio
 consultar a nota de um aluno digitando sua matricula, o programa deve permitir
-no m·ximo 10 consultas para um mesmo ciclo de execuÁ„o do programa*/
+no m√°ximo 10 consultas para um mesmo ciclo de execu√ß√£o do programa*/
 
 #include <stdio.h>
-#include <math.h>
 
 int main(){
-    int matricula[100],i,notas[100];
+    int i, matricula[100], notas[100];
     for ( i = 0; i < 100; i++)
     {
       
